592c11d54dfad75d620326fb2ef77ce878be8507
avazjanovsardorbek01 <sardoravazzanov@gmail.com>
2025-01-10 02:51:21 +0500

    Update-About

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -32,65 +32,78 @@ export default function Home() {
   return (
     <>
       {/* SECTION-1 STARTED */}
-      <section id="section-1" className="pb-[120px] sm:pb-[30px] sm:px-[20px]">
+      <section
+        id="section-1"
+        className="pb-[120px] sm:pb-[60px] md:pb-[80px] lg:pb-[100px] xl:pb-[120px] px-[20px] sm:px-[10px] md:px-[30px] lg:px-[40px] xl:px-[50px]"
+      >
         <div className="container">
           <div>
-            <Image src={AboutHero} alt="AboutHero" />
+            {/* Главное изображение */}
+            <Image
+              src={AboutHero}
+              alt="AboutHero"
+              className="w-full h-auto sm:h-[200px] md:h-[300px] lg:h-[400px] xl:h-[500px]"
+            />
 
-            <div className="shadow-lg rounded-b-[12px] sm:p-[0px] p-[20px]">
-              <div className="flex sm:flex-col sm:gap-[0px] gap-[95px]">
-                <div className="p-[40px] flex flex-col sm:gap-[4px] gap-[14px] items-center">
+            {/* Карточки статистики */}
+            <div className="shadow-lg rounded-b-[12px] p-[20px] sm:p-[10px] md:p-[15px] lg:p-[20px] xl:p-[30px]">
+              <div className="flex flex-wrap justify-center sm:flex-col sm:gap-[20px] md:gap-[30px] lg:gap-[50px] xl:gap-0 xl:grid xl:grid-cols-4">
+                {/* Карточка 1 */}
+                <div className="p-[40px] sm:p-[10px] md:p-[20px] lg:p-[30px] xl:p-[40px] flex flex-col gap-[14px] sm:gap-[10px] md:gap-[12px] lg:gap-[16px] xl:gap-[20px] items-center">
                   <Image
-                    className="sm:w-[20px]"
+                    className="sm:w-[20px] md:w-[30px] lg:w-[35px] xl:w-[40px]"
                     src={HomeHeart}
                     alt="home heart"
                   />
-
-                  <h1 className="text-[50px] sm:text-[18px] text-[#100E2C] font-[700] font-sans tracking-wide">
+                  <h1 className="text-[50px] sm:text-[24px] md:text-[30px] lg:text-[40px] xl:text-[50px] text-[#100E2C] font-[700] font-sans tracking-wide">
                     1250
                   </h1>
-
-                  <h3 className="text-[24px] sm:text-[12px] text-[#100E2C] font-[500] font-serif tracking-tight">
+                  <h3 className="text-[24px] sm:text-[12px] md:text-[16px] lg:text-[18px] xl:text-[24px] text-[#100E2C] font-[500] font-serif tracking-tight text-center">
                     Проданных объектов
                   </h3>
                 </div>
 
-                <div className="p-[40px] flex flex-col sm:gap-[4px] gap-[14px] items-center">
+                {/* Карточка 2 */}
+                <div className="p-[40px] sm:p-[10px] md:p-[20px] lg:p-[30px] xl:p-[40px] flex flex-col gap-[14px] sm:gap-[10px] md:gap-[12px] lg:gap-[16px] xl:gap-[20px] items-center">
                   <Image
-                    className="sm:w-[20px]"
+                    className="sm:w-[20px] md:w-[30px] lg:w-[35px] xl:w-[40px]"
                     src={Apartment}
                     alt="Apartment"
                   />
-
-                  <h1 className="text-[50px] sm:text-[18px] text-[#100E2C] font-[700] font-sans tracking-wide">
+                  <h1 className="text-[50px] sm:text-[24px] md:text-[30px] lg:text-[40px] xl:text-[50px] text-[#100E2C] font-[700] font-sans tracking-wide">
                     320
                   </h1>
-
-                  <h3 className="text-[24px] sm:text-[12px] text-[#100E2C] font-[500] font-serif tracking-tight">
+                  <h3 className="text-[24px] sm:text-[12px] md:text-[16px] lg:text-[18px] xl:text-[24px] text-[#100E2C] font-[500] font-serif tracking-tight text-center">
                     Проданных апартаментов
                   </h3>
                 </div>
 
-                <div className="p-[40px] flex flex-col sm:gap-[4px] gap-[14px] items-center">
-                  <Image className="sm:w-[20px]" src={Homes} alt="homes" />
-
-                  <h1 className="text-[50px] sm:text-[18px] text-[#100E2C] font-[700] font-sans tracking-wide">
+                {/* Карточка 3 */}
+                <div className="p-[40px] sm:p-[10px] md:p-[20px] lg:p-[30px] xl:p-[40px] flex flex-col gap-[14px] sm:gap-[10px] md:gap-[12px] lg:gap-[16px] xl:gap-[20px] items-center">
+                  <Image
+                    className="sm:w-[20px] md:w-[30px] lg:w-[35px] xl:w-[40px]"
+                    src={Homes}
+                    alt="homes"
+                  />
+                  <h1 className="text-[50px] sm:text-[24px] md:text-[30px] lg:text-[40px] xl:text-[50px] text-[#100E2C] font-[700] font-sans tracking-wide">
                     125
                   </h1>
-
-                  <h3 className="text-[24px] sm:text-[12px] text-[#100E2C] font-[500] font-serif tracking-tight">
+                  <h3 className="text-[24px] sm:text-[12px] md:text-[16px] lg:text-[18px] xl:text-[24px] text-[#100E2C] font-[500] font-serif tracking-tight text-center">
                     Проданных домов
                   </h3>
                 </div>
 
-                <div className="p-[40px] flex flex-col sm:gap-[4px] gap-[14px] items-center">
-                  <Image className="sm:w-[20px]" src={Zavod} alt="Zavod" />
-
-                  <h1 className="text-[50px] sm:text-[18px] text-[#100E2C] font-[700] font-sans tracking-wide">
+                {/* Карточка 4 */}
+                <div className="p-[40px] sm:p-[10px] md:p-[20px] lg:p-[30px] xl:p-[40px] flex flex-col gap-[14px] sm:gap-[10px] md:gap-[12px] lg:gap-[16px] xl:gap-[20px] items-center">
+                  <Image
+                    className="sm:w-[20px] md:w-[30px] lg:w-[35px] xl:w-[40px]"
+                    src={Zavod}
+                    alt="Zavod"
+                  />
+                  <h1 className="text-[50px] sm:text-[24px] md:text-[30px] lg:text-[40px] xl:text-[50px] text-[#100E2C] font-[700] font-sans tracking-wide">
                     120
                   </h1>
-
-                  <h3 className="text-[24px] sm:text-[12px] text-[#100E2C] font-[500] font-serif tracking-tight">
+                  <h3 className="text-[24px] sm:text-[12px] md:text-[16px] lg:text-[18px] xl:text-[24px] text-[#100E2C] font-[500] font-serif tracking-tight text-center">
                     Проданных вилл
                   </h3>
                 </div>
@@ -102,17 +115,25 @@ export default function Home() {
       {/* SECTION-1 ENDED */}
 
       {/* SECTION-2 STARTED */}
-      <section id="section-2" className="sm:pb-[40px]">
-        <div className="container sm:px-[20px] sm:pb-[40px] pb-[120px]">
-          <div className="flex sm:flex-col sm:pl-[0px] sm:gap-[40px] items-center gap-[134px] pl-[110px]">
-            <Image src={Expert} className="w-[416px]" alt="Expert" />
+      <section
+        id="section-2"
+        className="sm:pb-[40px] md:pb-[60px] lg:pb-[80px] xl:pb-[120px]"
+      >
+        <div className="container sm:px-[20px] md:px-[40px] lg:px-[50px] xl:px-[60px]">
+          <div className="flex sm:flex-col md:flex-row  sm:gap-[30px] md:gap-[70px] lg:gap-[90px] xl:gap-[120px] items-center">
+            {/* Изображение */}
+            <Image
+              src={Expert}
+              className="sm:w-[280px] md:w-[400px] lg:w-[450px] xl:w-[500px] rounded-lg shadow-xl"
+              alt="Expert"
+            />
 
-            <div className="w-[526px] sm:w-[350px]">
-              <h2 className="text-[#100E2C] sm:text-[20px] sm:pb-[20px] text-[40px] font-[700] pb-[40px]">
+            {/* Текстовый блок */}
+            <div className="sm:w-[100%] text-center md:w-[50%] p-5 lg:w-[526px]">
+              <h2 className="text-[#100E2C] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[40px] sm:pb-[20px] md:pb-[30px] lg:pb-[35px] xl:pb-[40px] font-bold leading-[1.4]">
                 Экспертные советы для всех ваших потребностей в недвижимости
               </h2>
-
-              <p className="sm:text-[14px]">
+              <p className="sm:text-[16px] md:text-[18px] lg:text-[20px] xl:text-[22px] text-[#5A5A5A] leading-[1.8]">
                 Преимущество заключается в том, что мы предоставляем вам
                 квалифицированную информацию по вопросам недвижимости и по
                 вопросам, которые могут возникнуть в процессе сделки.
@@ -126,61 +147,68 @@ export default function Home() {
       {/* SECTION-3 STARTED */}
       <section id="section-3" className="mb-[60px] sm:mb-[20px] sm:px-[20px]">
         <div className="container sm:pl-[0px] pl-[110px]">
-          <h2 className="text-[#100E2C] sm:text-[30px] sm:pb-[12px] text-[40px] text-center pb-[24px] font-[700]">
+          {/* Заголовок */}
+          <h2 className="text-[#100E2C] sm:text-[24px] md:text-[28px] lg:text-[34px] xl:text-[40px] text-center pb-[24px] sm:pb-[12px] font-[700]">
             Успех наших клиентов — наш успех.
           </h2>
 
-          <p className="text-center pb-[40px] sm:pb-[20px] sm:text-[14px]">
+          {/* Описание */}
+          <p className="text-center pb-[40px] sm:pb-[20px] sm:text-[14px] md:text-[16px] lg:text-[18px]">
             Наша цель — обеспечить вам лучший сервис, предоставляя экспертные
             рекомендации по недвижимости. Мы стремимся к тому, чтобы ваши
             инвестиции были максимально выгодными.
           </p>
 
-          <div className="flex sm:flex-col items-center gap-[24px]">
-            <div className="p-[40px] items-center w-[342px] text-[#fff] flex flex-col gap-[32px] rounded-[12px] bg-[#100E2C]">
+          {/* Карточки */}
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[24px]">
+            {/* Карточка 1 */}
+            <div className="p-[40px] sm:p-[20px] items-center text-[#fff] flex flex-col gap-[32px] rounded-[12px] bg-[#100E2C]">
               <Image src={HomeGroup} alt="home group" />
-              <h3 className="text-[24px] font-[700]">Купить недвижимость</h3>
-
-              <p className="text-center">
+              <h3 className="text-[24px] sm:text-[20px] font-[700] text-center">
+                Купить недвижимость
+              </h3>
+              <p className="text-center sm:text-[14px] md:text-[16px]">
                 Мы поможем вам найти лучшие варианты на рынке недвижимости.
                 Ознакомьтесь с актуальными предложениями.
               </p>
-
-              <button className="text-[20px] font-[500] bg-[#0057FF] rounded-[6px] w-[200px] h-[50px] hover:bg-[#0055ffdb] transition-all duration-200 active:bg-[#0057FF]">
+              <button className="text-[20px] sm:text-[16px] font-[500] bg-[#0057FF] rounded-[6px] w-full sm:w-[100%] md:w-[200px] h-[50px] hover:bg-[#0055ffdb] transition-all duration-200 active:bg-[#0057FF]">
                 Найти недвижимость
               </button>
             </div>
 
-            <div className="p-[40px] items-center w-[342px] text-[#fff] flex flex-col gap-[32px] rounded-[12px] bg-[#100E2C]">
+            {/* Карточка 2 */}
+            <div className="p-[40px] sm:p-[20px] items-center text-[#fff] flex flex-col gap-[32px] rounded-[12px] bg-[#100E2C]">
               <Image src={BankGroup} alt="Bank group" />
-              <h3 className="text-[24px] font-[700]">Продать недвижимость</h3>
-
-              <p className="text-center">
+              <h3 className="text-[24px] sm:text-[20px] font-[700] text-center">
+                Продать недвижимость
+              </h3>
+              <p className="text-center sm:text-[14px] md:text-[16px]">
                 Если вы хотите продать свою недвижимость, мы готовы предложить
                 вам лучшие условия для этого.
               </p>
-
-              <button className="text-[20px] font-[500] bg-[#0057FF] rounded-[6px] w-[200px] h-[50px] hover:bg-[#0055ffdb] transition-all duration-200 active:bg-[#0057FF]">
+              <button className="text-[20px] sm:text-[16px] font-[500] bg-[#0057FF] rounded-[6px] w-full sm:w-[100%] md:w-[200px] h-[50px] hover:bg-[#0055ffdb] transition-all duration-200 active:bg-[#0057FF]">
                 Узнать больше
               </button>
             </div>
 
-            <div className="p-[40px] items-center w-[342px] text-[#fff] flex flex-col gap-[32px] rounded-[12px] bg-[#100E2C]">
+            {/* Карточка 3 */}
+            <div className="p-[40px] sm:p-[20px] items-center text-[#fff] flex flex-col gap-[32px] rounded-[12px] bg-[#100E2C]">
               <Image src={HomeGroup2} alt="home group 2" />
-              <h3 className="text-[24px] font-[700]">Купить недвижимость</h3>
-
-              <p className="text-center">
+              <h3 className="text-[24px] sm:text-[20px] font-[700] text-center">
+                Купить недвижимость
+              </h3>
+              <p className="text-center sm:text-[14px] md:text-[16px]">
                 Мы предлагаем широкий выбор недвижимости для покупки и аренды с
                 разнообразными опциями для разных нужд.
               </p>
-
-              <button className="text-[20px] font-[500] bg-[#0057FF] rounded-[6px] w-[200px] h-[50px] hover:bg-[#0055ffdb] transition-all duration-200 active:bg-[#0057FF]">
+              <button className="text-[20px] sm:text-[16px] font-[500] bg-[#0057FF] rounded-[6px] w-full sm:w-[100%] md:w-[200px] h-[50px] hover:bg-[#0055ffdb] transition-all duration-200 active:bg-[#0057FF]">
                 Найти в аренду
               </button>
             </div>
           </div>
         </div>
       </section>
+
       {/* SECTION-3 ENDED */}
 
       {/* SECTION-4 STARTED */}
